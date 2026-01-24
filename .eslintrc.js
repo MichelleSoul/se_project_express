10c03bbbb189b6a4e93d335f41320108c5b9d9fd
Michelle Souliotis <michelle.souliotis@gmail.com>
2026-01-25 04:33:39 +0530

    Implement centralized middleware

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'no-underscore-dangle': 'off',
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
   },
 };
--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const mainRouter = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const errorHandler = require('./middlewares/error-handler');
 
 const app = express();
 
@@ -11,7 +15,15 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use(requestLogger);
+
 app.use('/', mainRouter);
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use(errorHandler);
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/wtwr_db')
